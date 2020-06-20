0bb339136b43722d58be3fbd419e33565a693060
Jean Lattus <jplattus@gmail.com>
2020-06-20 12:50:51 -0400

    feat: blog backend app init

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,48 @@
+import datetime
+
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 # Create your tests here.
+from mock import Mock
+
+from blog.models import Post
+from blog.serializers import PostSerializer
+
+
+class TestPostSerializer:
+
+    def test_expected_serialized_json(self):
+        post = Mock(spec=Post)
+
+        expected_results = {
+            'id': 1,
+            'title': 'Post 1',
+            'slug': 'post-1',
+            'author': 1,
+            'content': 'Lorem ipsum',
+            'updated_on': str(datetime.datetime.now()),
+            'created_on': str(datetime.datetime.now()),
+            'status': 0,
+        }
+
+        post = Post(**expected_results)
+
+        results = PostSerializer(post).data
+
+        assert results == expected_results
+
+    # def test_raise_error_when_missing_required_field(self):
+    #     incomplete_data = {
+    #         'name': 'Ferrari',
+    #         'code': 'fr1',
+    #         'created': str(datetime.datetime.now()),
+    #         'modified': str(datetime.datetime.now())
+    #     }
+    #
+    #     serializer = CarSerializer(data=incomplete_data)
+    #
+    #     # Este ContextManager nos permite verificar que
+    #     # se ejecute correctamente una Excepcion
+    #     with pytest.raises(ValidationError):
+    #         serializer.is_valid(raise_exception=True)
--- test_developer_nimbi/settings/local.py
+++ test_developer_nimbi/settings/local.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,11 +36,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Libs
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+
     # Apps
     'blog',
+    'analytics',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +56,21 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+REST_FRAMEWORK = {
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+
+}
 
 ROOT_URLCONF = 'test_developer_nimbi.urls'
 
@@ -73,7 +93,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'test_developer_nimbi.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -83,7 +102,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -103,20 +121,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
